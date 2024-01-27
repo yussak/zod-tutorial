b3f6515a7b02971385978c9b3204da96dac98d6e
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-27 14:48:55 +0900

    update

--- src/04-infer.problem.ts
+++ src/04-infer.problem.ts
@@ -10,8 +10,9 @@ const StarWarsPeopleResults = z.object({
   results: z.array(StarWarsPerson),
 });
 
-const logStarWarsPeopleResults = (data: unknown) => {
-  //                                    ^ 🕵️‍♂️
+
+const logStarWarsPeopleResults = (data: z.infer<typeof StarWarsPeopleResults>) => {
+  console.log(data)
   data.results.map((person) => {
     console.log(person.name);
   });
